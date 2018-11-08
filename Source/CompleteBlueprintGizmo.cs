59ae76829289977ee784db6d85eaaa5594be4488
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-07 23:55:36 -0800

    complete building button for Terrain and Blueprint_Install

--- Source/CompleteBlueprintGizmo.cs
+++ Source/CompleteBlueprintGizmo.cs
@@ -18,7 +18,7 @@ namespace TDBug
 
 			List<Gizmo> result = __result.ToList();
 
-			if(__instance is Blueprint || __instance is Frame)
+			if (__instance is Blueprint || __instance is Frame)
 				result.Add(new Command_Action()
 				{
 					defaultLabel = "Complete",
@@ -26,20 +26,39 @@ namespace TDBug
 					defaultDesc = "Complete this building",
 					action = delegate
 					{
+						Map map = __instance.Map;
+						Pawn builder = map.mapPawns.FreeColonistsSpawned.First(p =>
+						p.workSettings.WorkIsActive(WorkTypeDefOf.Construction) &&
+						(p.story == null || !p.story.WorkTypeIsDisabled(WorkTypeDefOf.Construction)));
+
 						if (__instance is Frame frame)
 						{
-							Pawn builder = frame.Map.mapPawns.FreeColonistsSpawned.First(p =>
-							p.workSettings.WorkIsActive(WorkTypeDefOf.Construction) &&
-							(p.story == null || !p.story.WorkTypeIsDisabled(WorkTypeDefOf.Construction)));
 							frame.CompleteConstruction(builder);
 						}
 
-						if(__instance is Blueprint_Build blueprint && blueprint.def.entityDefToBuild is ThingDef thingDef)
+						if (__instance is Blueprint_Build blueprint)
 						{
-							Thing thing = ThingMaker.MakeThing(thingDef, blueprint.UIStuff());
-							thing.SetFactionDirect(Faction.OfPlayer);
-							GenSpawn.Spawn(thing, blueprint.Position, blueprint.Map, blueprint.Rotation);
+							if (blueprint.def.entityDefToBuild is ThingDef thingDef)
+							{
+								Thing thing = ThingMaker.MakeThing(thingDef, blueprint.UIStuff());
+								thing.SetFactionDirect(Faction.OfPlayer);
+								GenSpawn.Spawn(thing, blueprint.Position, map, blueprint.Rotation);
+							}
+							else if (blueprint.def.entityDefToBuild is TerrainDef terrainDef)
+							{
+								map.terrainGrid.SetTerrain(blueprint.Position, terrainDef);
+								FilthMaker.RemoveAllFilth(blueprint.Position, map);
+							}
 							blueprint.Destroy(DestroyMode.Vanish);
+						}
+
+						if (__instance is Blueprint_Install install)
+						{
+							if(install.MiniToInstallOrBuildingToReinstall is Building building)
+							{
+								building.MakeMinified();
+							}
+							install.TryReplaceWithSolidThing(builder, out Thing thing, out bool ended);
 						}
 					}
 				});
