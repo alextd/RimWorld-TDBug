38f5b3192954a892eaa1292421c54661900b73f9
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-08 07:15:20 -0800

    No log limit

---
+++ Source/NoLogLimit.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+using Harmony;
+
+namespace TDBug
+{
+	[HarmonyPatch(typeof(Log), "ReachedMaxMessagesLimit", MethodType.Getter)]
+	class NoLogLimit
+	{
+		public static void Postfix(ref bool __result) => __result = false;
+	}
+}
