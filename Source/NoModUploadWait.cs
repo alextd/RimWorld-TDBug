564dcaf68341c7c0b974e754ffab12af0cea2aef
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-08 07:06:59 -0800

    Mods don't make you wait 6 seconds to upload

---
+++ Source/NoModUploadWait.cs
@@ -0,0 +1,11 @@
+﻿using Verse;
+using Harmony;
+
+namespace TDBug
+{
+	[HarmonyPatch(typeof(Dialog_MessageBox), "InteractionDelayExpired", MethodType.Getter)]
+	class NoModUploadWait
+	{
+		public static bool Postfix(bool __result) => true;
+	}
+}
