9d5f81298c8b52cf8de77668b520f7c5b77c0ad1
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-05 00:50:39 -0800

    Drawing readout for things matching specified id

--- Source/ThingById.cs
+++ Source/ThingById.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 namespace TDBug
 {
 	[HarmonyPatch(typeof(EditWindow_DebugInspector), nameof(EditWindow_DebugInspector.DoWindowContents))]
-	class ThingById
+	public static class ThingById_GUI
 	{
 		public static float headerHeight = 30f;
 		//public override void DoWindowContents(Rect inRect)
@@ -23,7 +23,6 @@ namespace TDBug
 				yield return i;
 				if (i.opcode == OpCodes.Ldc_R4 && (float)i.operand == headerHeight)
 				{
-					Log.Message("DID IT");
 					//Add another 30 pixels before readout to make room for id input (can't put it right of title, parent class controls that)
 					yield return new CodeInstruction(OpCodes.Ldc_R4, headerHeight);
 					yield return new CodeInstruction(OpCodes.Add);
@@ -37,6 +36,56 @@ namespace TDBug
 		{
 			Rect textRect = new Rect(inRect.x, inRect.y + headerHeight, inRect.width, headerHeight);
 			Widgets.TextFieldNumeric(textRect, ref idToFind, ref idBuffer);
+		}
+	}
+
+	[HarmonyPatch(typeof(EditWindow_DebugInspector), "CurrentDebugString")]
+	public static class ThingById_Readout
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			FieldInfo fullModeInfo = AccessTools.Field(typeof(EditWindow_DebugInspector), nameof(EditWindow_DebugInspector.fullMode));
+
+			bool foundfullMode = false;
+
+			foreach (CodeInstruction i in instructions)
+			{
+				yield return i;
+				//ldfld        bool Verse.EditWindow_DebugInspector::fullMode
+				if (i.opcode == OpCodes.Ldfld && i.operand == fullModeInfo)
+					foundfullMode = true;
+
+				//Pop comes after stringBuilder.AppendLine("---");
+				if (i.opcode == OpCodes.Pop && foundfullMode)
+				{
+					foundfullMode = false;
+
+					yield return new CodeInstruction(OpCodes.Ldloc_0); //local StringBuilder ; todo: find it better
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ThingById_Readout), nameof(ReadoutFoundThings)));
+				}
+			}
+		}
+
+		public static void ReadoutFoundThings(StringBuilder sb)
+		{
+			foreach (Thing thing in FindThings(ThingById_GUI.idToFind))
+			{
+				sb.AppendLine(Scribe.saver.DebugOutputFor(thing));
+				sb.AppendLine();
+			}
+		}
+
+		public static IEnumerable<Thing> FindThings(int id)
+		{
+			if (Current.Game == null) yield break;
+			foreach (Map map in Current.Game.Maps)
+				foreach (Thing thing in map.listerThings.AllThings)
+					if (thing.thingIDNumber == id)
+						yield return thing;
+			foreach (Thing thing in Current.Game.World.worldPawns.AllPawnsAliveOrDead)
+				if (thing.thingIDNumber == id)
+					yield return thing;
+			//Todo: inner containers. or just somehow ALL THINGS
 		}
 	}
 }
