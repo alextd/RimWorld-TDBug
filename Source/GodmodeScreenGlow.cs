bff56e39d1d500f1ab1ea6972971a9c1b32eb995
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-06 09:03:05 -0800

    Why not also put a checkmark on the godmode icon

--- Source/GodmodeScreenGlow.cs
+++ Source/GodmodeScreenGlow.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
 using Verse;
 using Harmony;
 using UnityEngine;
 
 namespace TDBug
 {
-	[HarmonyPatch(typeof(DebugWindowsOpener), "DevToolStarterOnGUI")]
+	[HarmonyPatch(typeof(DebugWindowsOpener), nameof(DebugWindowsOpener.DevToolStarterOnGUI))]
+	[StaticConstructorOnStartup]
 	class GodmodeScreenGlow
 	{
 		public static Texture2D gradientTex = ContentFinder<Texture2D>.Get("UI/Overlays/TargetHighlight_Side"); 
@@ -25,6 +28,34 @@ namespace TDBug
 				//Widgets.DrawTextureRotated(new Rect(0, 0, 50f, Screen.height), gradientTex, -90);
 				//Widgets.DrawTextureRotated(new Rect(Screen.width - 50f, 0, 50f, Screen.height), gradientTex, 90);
 			}
+		}
+	}
+	[HarmonyPatch(typeof(DebugWindowsOpener), "DrawButtons")]
+	public static class HighlightButton
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo ButtonIconInfo = AccessTools.Method(typeof(WidgetRow), nameof(WidgetRow.ButtonIcon));
+			MethodInfo ToggleGodModeInfo = AccessTools.Method(typeof(DebugWindowsOpener), "ToggleGodMode");
+
+			List<CodeInstruction> instList = instructions.ToList();
+			for(int i=0; i<instList.Count(); i++)
+			{
+				CodeInstruction inst = instList[i];
+				if(inst.opcode == OpCodes.Callvirt && inst.operand == ButtonIconInfo
+					&& instList[i+3].opcode == OpCodes.Call && instList[i+3].operand == ToggleGodModeInfo)
+				{
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(HighlightButton), nameof(ActuallyToggleableIcon)));
+					yield return new CodeInstruction(OpCodes.Br, instList[++i].operand);
+				}
+				else
+					yield return inst;
+			}
+		}
+
+		public static void ActuallyToggleableIcon(WidgetRow row, Texture2D tex, string tooltip, Color? mouseoverColor)
+		{
+			row.ToggleableIcon(ref DebugSettings.godMode, tex, "tooltip", null, null);
 		}
 	}
 }
