0e158e935acf4b230313d3d2ddcbdcbd1b3fb48e
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-06 07:41:49 -0800

    Saving debug window sizes : log, tweak values, and inspector

---
+++ Source/SaveDebugWindowRect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+using UnityEngine;
+
+namespace TDBug
+{
+	[HarmonyPatch(typeof(Window), "SetInitialSizeAndPosition")]
+	public static class LoadWindowSize
+	{
+		public static HashSet<Type> windowTypes = new HashSet<Type>()
+		{ typeof(EditWindow_Log), typeof(EditWindow_TweakValues), typeof(EditWindow_DebugInspector) };
+		public static Dictionary<Type, Rect> savedSizes = new Dictionary<Type, Rect>();
+
+		//protected virtual void SetInitialSizeAndPosition()
+		public static void Postfix(Window __instance)
+		{
+			Type windowType = __instance.GetType();
+			if (savedSizes.TryGetValue(windowType, out Rect savedRect))
+			{
+				__instance.windowRect = savedRect;
+				savedSizes.Remove(windowType);
+			}
+		}
+	}
+	
+	[HarmonyPatch(typeof(Window), "PreClose")]
+	public static class SaveWindowSize
+	{
+		public static void Prefix(Window __instance)
+		{
+			Type windowType = __instance.GetType();
+			if(LoadWindowSize.windowTypes.Contains(windowType))
+				LoadWindowSize.savedSizes[windowType] = __instance.windowRect;
+		}
+	}
+}
