e8484307aa15aff890952e4563d3d24be2ddaf30
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-22 21:25:02 -0700

    Make roof by def debug tool

--- Source/MoreDebugActions.cs
+++ Source/MoreDebugActions.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using Verse;
 using Harmony;
+using RimWorld;
 
 namespace TDBug
 {
@@ -22,13 +23,15 @@ namespace TDBug
 		public static Dictionary<string, DA> insertAfter = new Dictionary<string, DA>() {
 			{ "T: Try place near stacks of 75...", new DA() {label = "T: Try place near full stacks...", action = "fullStackAction"} },
 			{ "Destroy all things", new DA() {label = "Destroy all selected", action = "destroySelectedAction"} },
-			{ "T: Heal random injury (10)", new DA() {label = "T: Full Heal", action = "healFullAction", tool="DebugToolMapForPawns" } }
+			{ "T: Heal random injury (10)", new DA() {label = "T: Full Heal", action = "healFullAction", tool="DebugToolMapForPawns" } },
+			{ "T: Make roof", new DA() {label = "T: Make roof (by def)", action = "makeRoofByDef"} }
 		};
 		
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
 		{
 			//check after either
 			MethodInfo DebugActionInfo = AccessTools.Method(typeof(Dialog_DebugOptionLister), "DebugAction");
+			MethodInfo DebugToolMapInfo = AccessTools.Method(typeof(Dialog_DebugOptionLister), "DebugToolMap");
 			MethodInfo DebugToolMapForPawnsInfo = AccessTools.Method(typeof(Dialog_DebugOptionLister), "DebugToolMapForPawns");
 
 			List<CodeInstruction> instList = instructions.ToList();
@@ -39,7 +42,7 @@ namespace TDBug
 				yield return inst;
 
 				if (inst.opcode == OpCodes.Call && 
-					(inst.operand == DebugActionInfo || inst.operand == DebugToolMapForPawnsInfo))
+					(inst.operand == DebugActionInfo || inst.operand == DebugToolMapForPawnsInfo || inst.operand == DebugToolMapInfo))
 				{
 					if (instList[i - 8].opcode == OpCodes.Ldstr && instList[i - 8].operand is string afterLabel)
 					{
@@ -68,6 +71,10 @@ namespace TDBug
 		public static MethodInfo DebugToolMapForPawnsInfo = AccessTools.Method(typeof(Dialog_DebugOptionLister), "DebugToolMapForPawns");
 		public static void DebugToolMapForPawns(Dialog_DebugOptionLister dialog, string label, Action<Pawn> action) =>
 			DebugToolMapForPawnsInfo.Invoke(dialog, new object[] { label, action });
+
+		public static MethodInfo DebugToolMapInfo = AccessTools.Method(typeof(Dialog_DebugOptionLister), "DebugToolMap");
+		public static void DebugToolMap(Dialog_DebugOptionLister dialog, string label, Action action) =>
+			DebugToolMapInfo.Invoke(dialog, new object[] { label, action });
 
 		//what 'action' above refers to
 		public static Action fullStackAction = delegate
@@ -102,6 +109,25 @@ namespace TDBug
 				hediff_Injury.Heal(10000f);//probably enough
 
 			}
+		};
+		public static Action makeRoofByDef = delegate ()
+		{
+			List<DebugMenuOption> list = new List<DebugMenuOption>();
+			foreach (RoofDef current in DefDatabase<RoofDef>.AllDefs)
+			{
+				RoofDef localDef = current;
+				list.Add(new DebugMenuOption(localDef.LabelCap, DebugMenuOptionMode.Tool, delegate
+				{
+					CellRect.CellRectIterator iterator = CellRect.CenteredOn(UI.MouseCell(), 1).GetIterator();
+					while (!iterator.Done())
+					{
+						Find.CurrentMap.roofGrid.SetRoof(iterator.Current, localDef);
+						iterator.MoveNext();
+					}
+				}));
+			}
+
+			Find.WindowStack.Add(new Dialog_DebugOptionListLister(list));
 		};
 	}
 }
