9c9e80f23a286512cf6c204511b827dbb3abfca0
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-08-15 14:05:59 -0700

    1.2 Brought debug menu by keyboard. It didn't bring in highlighting exact matches (/kill) or out of order (points incident).
    These features are RESTORED. A bit of copy/paste into new files.

---
+++ Source/DebugActionFilterMultiWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace TDBug
+{
+	//The original filter method checked an exact substring so "pawn spawn" wouldn't find "spawn pawn"
+	//Let's separate by word
+	[HarmonyPatch(typeof(Dialog_OptionLister), "FilterAllows")]
+	static class FilterSplitOnSpace
+	{
+		//protected bool FilterAllows(string label)
+		static void Postfix(ref bool __result, string label, string ___filter)
+		{
+			if (__result) return;
+
+			if (___filter.Contains(' ') || ___filter.Contains('\t'))
+				__result = ___filter.Split(' ', '\t').All(
+				f => label.IndexOf(f, StringComparison.OrdinalIgnoreCase) >= 0);
+		}
+	}
+}
---
+++ Source/DebugActionHighlightExactMatch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace TDBug
+{
+	[HarmonyPatch(typeof(Dialog_DebugActionsMenu), "HighlightedIndex", MethodType.Getter)]
+	//protected override int HighlightedIndex
+	static class DebugActionHighlightExactMatch
+	{
+		public static MethodInfo FilterAllowsInfo = AccessTools.Method(typeof(Dialog_OptionLister), "FilterAllows");
+		public static bool FilterAllows(Dialog_DebugOptionLister window, string label) =>
+			(bool)FilterAllowsInfo.Invoke(window, new object[] { label });
+
+		public static FieldInfo filterInfo = AccessTools.Field(typeof(Dialog_OptionLister), "filter");
+		public static string GetFilter(this Dialog_OptionLister window) =>
+			(string)filterInfo.GetValue(window);
+
+		public static bool FilterExactlyMatches(string filter, string label)
+		{
+			if (label.StartsWith("T: "))
+				label = label.Substring(3);
+			return String.Equals(filter, label, StringComparison.OrdinalIgnoreCase);
+		}
+
+		//After getting highlighted index, check if it exactly matches one and highlight that instead.
+		public static void Postfix(ref int __result, Dialog_DebugActionsMenu __instance, List<Dialog_DebugActionsMenu.DebugActionOption> ___debugActions, ref int ___prioritizedHighlightedIndex)
+		{
+			if (FilterExactlyMatches(__instance.GetFilter(), ___debugActions[__result].label))
+				return;
+
+			for (int i = 0; i < ___debugActions.Count; i++)
+			{
+				if (FilterExactlyMatches(__instance.GetFilter(), ___debugActions[i].label))
+				{
+					___prioritizedHighlightedIndex = i;
+					__result = i;
+					return;
+				}
+			}
+		}
+	}
+}
