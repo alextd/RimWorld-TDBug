9cd855416e1ee2524bc9b2ad9984ab0eb87ea607
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-08-30 13:19:48 -0700

    Seems the DefOf warnings are not a problem anymore. Harmony patches going in mod load as usual

--- Source/TDBug.cs
+++ Source/TDBug.cs
@@ -7,21 +7,17 @@ using RimWorld;
 
 namespace TDBug
 {
-	[StaticConstructorOnStartup]
 	public class Mod : Verse.Mod
 	{
 		public Mod(ModContentPack content) : base(content)
 		{
-			//TODO: patches before load. Probably won't be needed in TDBug. Patching here causes DefOf warnings.
-		}
-
-		static Mod()
-		{
 #if DEBUG
 			Harmony.DEBUG = true;
 #endif
+
 			Harmony harmony = new Harmony("Uuugggg.rimworld.TDBug.main");
-			LongEventHandler.QueueLongEvent(() => harmony.PatchAll(), null, true, null);
+
+			harmony.PatchAll();
 		}
 	}
 }
