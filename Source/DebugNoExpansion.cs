2ed4c6ddd528dcaf8d99554a20ee35f166f052f6
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-09-26 11:02:00 -0700

    Don't require expansions to do debug actions. Of course things will be bugged without some textures or whatnot.

---
+++ Source/DebugNoExpansion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+
+namespace TDBug
+{
+	[HarmonyPatch(typeof(DebugActionAttribute), nameof(DebugActionAttribute.IsAllowedInCurrentGameState), MethodType.Getter)]
+	public static class DebugNoExpansion
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			FieldInfo requiresRoyaltyInfo = AccessTools.Field(typeof(DebugActionAttribute), nameof(DebugActionAttribute.requiresRoyalty));
+			FieldInfo requiresIdeologyInfo = AccessTools.Field(typeof(DebugActionAttribute), nameof(DebugActionAttribute.requiresIdeology));
+
+			foreach (var inst in instructions)
+			{
+				if (inst.LoadsField(requiresIdeologyInfo) || inst.LoadsField(requiresRoyaltyInfo))
+				{
+					//this.requiresIdeology -> false
+					yield return new CodeInstruction(OpCodes.Pop);//remove `this`
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0);//false
+				}
+				else
+					yield return inst;
+			}
+		}
+	}
+}
