67d941aa797939cbfa82ae9ce59664ffcc028298
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-10-06 13:48:54 -0700

    Mouseover readout showed over fogged with godmode

---
+++ Source/MouseoverReadoutFogged.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace TDBug
+{
+	//public void MouseoverReadoutOnGUI()
+	[HarmonyPatch(typeof(MouseoverReadout), nameof(MouseoverReadout.MouseoverReadoutOnGUI))]
+	public static class MouseoverReadoutFogged
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			return HarmonyLib.Transpilers.MethodReplacer(instructions,
+				AccessTools.Method(typeof(GridsUtility), nameof(GridsUtility.Fogged), new Type[] { typeof(IntVec3), typeof(Map) }),
+				AccessTools.Method(typeof(MouseoverReadoutFogged), nameof(FoggedAndNotDebug)));
+		}
+		//public static bool Fogged(this IntVec3 c, Map map)
+		public static bool FoggedAndNotDebug(IntVec3 c, Map map)
+		{
+			if (DebugSettings.godMode) return false;
+
+			return c.Fogged(map);
+		}
+	}
+}
