e61af7a63cb731df033a23db9887eb81686009ff
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-10-15 13:23:30 -0700

    Set disabled mods titles to white. They're already in the disabled LIST, they don't need to be grey.

---
+++ Source/ModsConfigNameNotGrey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using UnityEngine;
+
+namespace TDBug
+{
+	[HarmonyPatch(typeof(Page_ModsConfig), nameof(Page_ModsConfig.DoModRow))]
+	public static class ModsConfigNameNotGrey
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo greyInfo = AccessTools.PropertyGetter(typeof(Color), nameof(Color.grey));
+			MethodInfo whiteInfo = AccessTools.PropertyGetter(typeof(Color), nameof(Color.white));
+			foreach (var inst in instructions)
+			{
+				if (inst.Calls(greyInfo))
+					inst.operand = whiteInfo;
+				yield return inst;
+			}
+		}
+	}
+}
