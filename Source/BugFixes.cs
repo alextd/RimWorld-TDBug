2af202b2e2fa4201d7fccab470757f8e2f313ff2
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-11-24 12:47:35 -0800

    Fix WidgetRow.Label to not Gap when curX == 0

--- Source/BugFixes.cs
+++ Source/BugFixes.cs
@@ -11,6 +11,44 @@ using UnityEngine;
 
 namespace TDBug
 {
+	// WidgetRow.Label and TextFieldNumeric apply a gap before and after the rect it uses
+	// But instead of Gap() it directly calls IncrementPosition, without checking if startX==curX
+	// So labels at the beginning of a widgetRow on somr rect would have a gap applied
+	//  (whereas a similar Widgets.Label in the same rect does not)
+	// Which is really funny, because caling Gap() first explicitly doesn't create a gap, but label does create a gap
+
+	//  so TL;DR the first IncrementPosition in these methods should be Gap so it doesn't actually gap when curX == 0
+
+	[HarmonyPatch(typeof(WidgetRow), nameof(WidgetRow.Label))]
+	public static class FixWidgetRowLabelGap
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo IncrementPositionInfo = AccessTools.Method(typeof(WidgetRow), nameof(WidgetRow.IncrementPosition));
+			MethodInfo GapInfo = AccessTools.Method(typeof(WidgetRow), nameof(WidgetRow.Gap));
+
+			bool first = true;
+			foreach (var inst in instructions)
+			{
+				if(first && inst.Calls(IncrementPositionInfo))
+				{
+					first = false;
+					inst.operand = GapInfo;
+				}
+				yield return inst;
+			}
+		}
+	}
+
+	/*
+	//TextFieldNumeric is generic and crashes rimworld on start
+	[HarmonyPatch(typeof(WidgetRow), nameof(WidgetRow.TextFieldNumeric))]
+	public static class FixWidgetRowTextFieldNumericGap
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+			=> FixWidgetRowLabelGap.Transpiler(instructions);
+	}
+	*/
 
 
 	// WidgetRow.ButtonText does A Text.CalcSize to find the size of the button ; that height ends up 22.
