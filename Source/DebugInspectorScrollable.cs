8e800f1efef51daf99a43cc223d028bd9bc84fab
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2024-03-13 17:56:04 -0700

    Rewrite scrolling inspector : aka, copy code from 1.4 that was rewritten for 1.5, and insert the patches in-place

--- Source/DebugInspectorScrollable.cs
+++ Source/DebugInspectorScrollable.cs
@@ -12,16 +12,8 @@ namespace TDBug
 {
 	public static class Listing_StandardExtensions
 	{
-		//listing.listingRect = rect;
-		public static FieldInfo rectInfo = AccessTools.Field(typeof(Listing_Standard), "listingRect");
-		//listing.columnWidthInt = listing.listingRect.width;
-		public static FieldInfo widthInfo = AccessTools.Field(typeof(Listing_Standard), "columnWidthInt");
-		//listing.curX = 0f;
-		public static FieldInfo curXInfo = AccessTools.Field(typeof(Listing_Standard), "curX");
-		//listing.curY = 0f;
-		public static FieldInfo curYInfo = AccessTools.Field(typeof(Listing_Standard), "curY");
 		public static FieldInfo fontInfo = AccessTools.Field(typeof(Listing_Standard), "font");
-		public static void BeginScrollViewEx(this Listing_Standard listing, Rect rect, ref Vector2 scrollPosition, ref Rect viewRect)
+		public static void BeginScrollViewEx(this Listing_Standard listing, Rect rect, ref Vector2 scrollPosition, Rect viewRect)
 		{
 			//Widgets.BeginScrollView(rect, ref scrollPosition, viewRect, true);
 			//rect.height = 100000f;
@@ -38,61 +30,101 @@ namespace TDBug
 			//base.Begin(rect.AtZero());
 
 
-			//listing.listingRect = rect;
-			rectInfo.SetValue(listing, rect);
-			//listing.columnWidthInt = listing.listingRect.width;
-			widthInfo.SetValue(listing, rect.width);
-			//listing.curX = 0f;
-			curXInfo.SetValue(listing, 0);
-			//listing.curY = 0f;
-			curYInfo.SetValue(listing, 0);
+			listing.listingRect = rect;
+			listing.columnWidthInt = rect.width;
+			listing.curX = 0f;
+			listing.curY = 0f;
 
-			Text.Font = (GameFont)fontInfo.GetValue(listing);
+
+			Text.Font = listing.font;
 		}
 	}
 
+	//TOOD: Remove the "Make the columns wider" buttons
 	[HarmonyPatch(typeof(EditWindow_DebugInspector), nameof(EditWindow_DebugInspector.DoWindowContents))]
 	public static class DebugInspectorScrollable
 	{
 		//public override void DoWindowContents(Rect inRect)
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
 		{
-			//dunno how this isn't Listing_Standard but apparently it's Listing in ILCode 
-			MethodInfo beginInfo = AccessTools.Method(typeof(Listing), nameof(Listing.Begin));
-			MethodInfo endInfo = AccessTools.Method(typeof(Listing), nameof(Listing.End));
+			//1.5 removed listing and manually adjusts the x and y shit, so let's just rewrite that section of code entirely
+			//
+			//
+
+			// Replace everything after the SECOND:
+			//    IL_0145: call         instance void [UnityEngine.CoreModule]UnityEngine.Rect::set_yMin(float32)
+
+			MethodInfo setYMinInfo = AccessTools.PropertySetter(typeof(Rect), nameof(Rect.yMin));
+			bool calledOnce = false;
 
 			foreach (CodeInstruction i in instructions)
 			{
-				//listing_Standard.Begin(inRect);
-				if (i.Calls(beginInfo))
+				yield return i;
+
+				if(i.Calls(setYMinInfo))
 				{
-					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DebugInspectorScrollable), nameof(BeginScroll)));
+					if (calledOnce)
+					{
+						// this
+						yield return new CodeInstruction(OpCodes.Ldarg_0);
+						// inRect
+						yield return new CodeInstruction(OpCodes.Ldarg_1);
+
+						// ref this.debugStringBuilder
+						yield return new CodeInstruction(OpCodes.Ldarg_0); //this
+						yield return new CodeInstruction(OpCodes.Ldflda, AccessTools.Field(typeof(EditWindow_DebugInspector), nameof(EditWindow_DebugInspector.debugStringBuilder)));
+
+						// ReWriteInspectorList(this, inRect, ref this.debugStringBuilder)
+						yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DebugInspectorScrollable), nameof(ReWriteInspectorList)));
+
+						//I guess this is needed
+						yield return new CodeInstruction(OpCodes.Ret);
+
+
+						//Kill the rest as I just rewrote it in ReWriteInspectorList
+						yield break;
+					}
+					else
+						calledOnce = true;
 				}
-				else if (i.Calls(endInfo))
-				{
-					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DebugInspectorScrollable), nameof(EndScroll)));
-				}
-				else 
-					yield return i;
 			}
 		}
 
+		public static void ReWriteInspectorList(EditWindow_DebugInspector instance, Rect inRect, ref StringBuilder debugStringBuilder)
+		{
+			//from 1.4 code:
+			Listing_Standard listing_Standard = new Listing_Standard(GameFont.Tiny);
+			listing_Standard.ColumnWidth = Mathf.Min(360, inRect.width);
+			BeginScroll(listing_Standard, inRect);//<- previously just replaced this
+			string[] array = debugStringBuilder.ToString().Split('\n');
+			foreach (string label in array)
+			{
+				listing_Standard.Label(label);
+				listing_Standard.Gap(-9f);
+			}
+			EndScroll(listing_Standard);//< -previously just replaced this
+
+			//TODO: this is unchanged 1.5 but it'd be harder to NOT remove it from ilcode.
+			if (Event.current.type == EventType.Repaint)
+			{
+				debugStringBuilder = new StringBuilder();
+				debugStringBuilder.Append(instance.CurrentDebugString());
+			}
+		}
+
+
 		public static Vector2 scrollPosition;
-		public static Rect viewRect;
 		public static float scrollViewHeight;
 		public static void BeginScroll(Listing_Standard listing, Rect rect)
 		{
-			viewRect = new Rect(0f, 0f, rect.width - 16f, scrollViewHeight);
-			listing.BeginScrollViewEx(rect, ref scrollPosition, ref viewRect);
+			Rect viewRect = new Rect(0f, 0f, rect.width - 16f, scrollViewHeight);
+			listing.BeginScrollViewEx(rect, ref scrollPosition, viewRect);
 		}
 		public static void EndScroll(Listing_Standard listing)
 		{
-			//listing.EndScrollView(ref viewRect); //1.3 removed this
-			viewRect = new Rect(0f, 0f, listing.ColumnWidth, listing.CurHeight);
+			scrollViewHeight = listing.CurHeight;
 			Widgets.EndScrollView();
 			listing.End();
-
-			scrollViewHeight = viewRect.height;
 		}
 	}
 }
